8917ea68800f4445e6b086984446ed10ec94f59e
Edgar Arroyo <Edgar@iMac-de-Edgar.local>
2016-10-22 23:51:05 -0500

    new db

--- public/dbubu/ubu.sql
+++ public/dbubu/ubu.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 21-10-2016 a las 06:26:15
+-- Tiempo de generación: 23-10-2016 a las 06:50:41
 -- Versión del servidor: 10.1.16-MariaDB
 -- Versión de PHP: 5.5.38
 
@@ -49,7 +49,10 @@ INSERT INTO `comentarios-codigos` (`id`, `user_id`, `codigo_id`, `comentario`) V
 (24, 15, 3, 'that''s nice'),
 (25, 15, 10, 'yay'),
 (26, 15, 6, 'muy simple'),
-(27, 15, 6, 'Excelente');
+(27, 15, 6, 'Excelente'),
+(28, 23, 11, 'dasds'),
+(29, 23, 13, 'comentario'),
+(30, 23, 15, 'hola');
 
 -- --------------------------------------------------------
 
@@ -73,7 +76,8 @@ CREATE TABLE `groups` (
 
 INSERT INTO `groups` (`id`, `name`, `type`, `privacy`, `created_by`, `created_at`, `updated_at`) VALUES
 (3, 'Team Ubu', 'Soporte', NULL, 8, '2016-05-13 00:49:45', NULL),
-(4, 'equipo1', 'poo', NULL, 11, '2016-05-13 13:35:52', NULL);
+(4, 'equipo1', 'poo', NULL, 11, '2016-05-13 13:35:52', NULL),
+(5, 'GrupoPrueba', 'Probando los grupos', NULL, 23, '2016-10-23 04:22:25', NULL);
 
 -- --------------------------------------------------------
 
@@ -85,18 +89,20 @@ CREATE TABLE `groups_members` (
   `id` int(11) NOT NULL,
   `group_id` int(11) NOT NULL,
   `member_id` int(11) NOT NULL,
-  `added_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `added_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `admin` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `groups_members`
 --
 
-INSERT INTO `groups_members` (`id`, `group_id`, `member_id`, `added_at`) VALUES
-(1, 3, 8, '2016-05-13 00:49:45'),
-(2, 3, 10, '2016-05-13 03:09:08'),
-(3, 3, 11, '2016-05-13 03:09:08'),
-(9, 3, 15, '2016-10-21 04:18:12');
+INSERT INTO `groups_members` (`id`, `group_id`, `member_id`, `added_at`, `admin`) VALUES
+(2, 3, 8, '2016-10-23 04:33:21', 1),
+(3, 3, 11, '2016-05-13 03:09:08', 0),
+(9, 3, 15, '2016-10-21 04:18:12', 0),
+(13, 5, 23, '2016-10-23 04:48:48', 1),
+(21, 5, 8, '2016-10-23 04:48:57', 0);
 
 -- --------------------------------------------------------
 
@@ -116,7 +122,9 @@ CREATE TABLE `grupos-notas` (
 --
 
 INSERT INTO `grupos-notas` (`id`, `user_id`, `nota`, `grupo_id`) VALUES
-(9, 8, 'Arre!', 3);
+(9, 8, 'Arre!', 3),
+(11, 23, 'hola', 3),
+(12, 23, 'hola', 3);
 
 -- --------------------------------------------------------
 
@@ -203,7 +211,8 @@ INSERT INTO `likes` (`interaction_id`, `post_id`, `user_id`) VALUES
 (89, 66, 8),
 (90, 65, 8),
 (91, 65, 8),
-(92, 65, 8);
+(92, 65, 8),
+(93, 68, 8);
 
 -- --------------------------------------------------------
 
@@ -268,7 +277,11 @@ INSERT INTO `post-codigos` (`id`, `user_id`, `codigo`, `sintaxis`, `titulo`, `gr
 (8, 15, '<?php\n\nnamespace App\\Http\\Controllers;\n\nuse App\\Http\\Controllers\\Controller;\nuse Request;\nuse DB;\n\nclass EditorController extends Controller\n{\n  public function getView($id){\n    session_start();\n    $get_code = DB::table(''post-codigos'')->where(''id'',''='',$id)->get();\n    if($get_code != null){\n      $_SESSION[''code_id''] = $id;\n      return view(''code_view'');\n    }\n  }\n\n  public function getInfo(){\n    session_start();\n    $code_id = $_SESSION[''code_id''];\n    $get_code = DB::table(''post-codigos'')->where(''id'',''='',$code_id)->get();\n    return $get_code;\n  }\n\n  public function post_comment(){\n    session_start();\n    $comentario = Request::input(''comentario'');\n    $post = DB::table(''comentarios-codigos'')\n    ->insert([\n      ''user_id'' => $_SESSION[''uid''],\n      ''codigo_id'' => $_SESSION[''code_id''],\n      ''comentario'' => $comentario,\n    ]);\n\n    return 200;\n  }\n\n  public function get_comments(){\n    session_start();\n    $get_comment = DB::table(''comentarios-codigos'')\n    ->join(''users'',''comentarios-codigos.user_id'',''='',''users.id'')\n    ->join(''user-data'',''users.id'',''='',''user-data.user_id'')\n    ->select(''users.name'',''user-data.profile_picture'',''comentarios-codigos.comentario'',''comentarios-codigos.codigo_id'')\n    ->where(''comentarios-codigos.codigo_id'',''='',$_SESSION[''code_id''])\n    ->orderBy(''comentarios-codigos.id'',''desc'')\n    ->get();\n    return $get_comment;\n  }\n\n  public function related_codes(){\n    session_start();\n    $id_publicador = DB::table(''post-codigos'')->select(''user_id'')\n    ->where(''id'',''='',$_SESSION[''code_id''])->get();\n    $codigos = DB::table(''post-codigos'')->where(''user_id'',''='',$id_publicador[0]->user_id)->get();\n    return $codigos;\n  }\n}', 'php', 'EditorController', NULL),
 (9, 15, '<?php\n\nnamespace App\\Http\\Controllers;\n\nuse App\\Http\\Controllers\\Controller;\nuse Request;\nuse DB;\n\nclass FeedController extends Controller\n{\n    public function get_post_follows(){\n      session_start();\n      $follows = DB::table(''suscribciones'')->select(''suscripcion_id'')\n      ->where(''suscriptor_id'',''='',$_SESSION[''uid''])->get();\n      foreach ($follows as $numero => $valor) {\n      $personas[$numero] = $follows[$numero]->suscripcion_id;\n      }\n      $personas[] = $_SESSION[''uid''];\n\n      $getpost = DB::table(''posts'')\n      ->join(''users'',''posts.id_user'',''='',''users.id'')\n      ->join(''user-data'',''users.id'',''='',''user-data.user_id'')\n      ->join(''users as MU'',''posts.mencion'',''='',''MU.id'')\n      ->select(''posts.id_user'',''posts.mencion'',''users.name'',''posts.text'', ''posts.id'',''user-data.profile_picture'',\n        ''MU.name as mname'',''posts.likes'')\n      ->whereIn(''posts.id_user'',$personas)\n      ->orderBy(''posts.created_at'',''desc'')->get();\n\n      return $getpost;\n    }\n\n    public function get_info(){\n      session_start();\n      $profile = $_SESSION[''uid''];\n      $getinfo = DB::table(''users'')\n      ->join(''user-data'',''users.id'',''='',''user-data.user_id'')\n      ->select(''users.name'',''users.id'',''user-data.profile_picture'',\n      ''user-data.profile_cover'',''user-data.Twitter''\n      )->where(''users.id'',''='',$profile)->get();\n       return $getinfo;\n    }\n\n    public function get_codes(){\n      session_start();\n      $follows = DB::table(''suscribciones'')->select(''suscripcion_id'')\n      ->where(''suscriptor_id'',''='',$_SESSION[''uid''])->get();\n      foreach ($follows as $numero => $valor) {\n      $personas[$numero] = $follows[$numero]->suscripcion_id;\n      }\n\n\n      $code_post = DB::table(''post-codigos'')\n      ->join(''users'',''post-codigos.user_id'',''='',''users.id'')\n      ->join(''user-data'',''users.id'',''='',''user-data.user_id'')\n      ->select(''post-codigos.id'',''post-codigos.sintaxis'',''post-codigos.titulo'',\n      ''users.name'',''user-data.profile_picture'',''post-codigos.user_id''\n      )->whereIn(''post-codigos.user_id'',$personas)\n      ->orderBy(''post-codigos.id'',''desc'')->get();\n      return $code_post;\n    }\n}', 'php', 'FeedController', NULL),
 (10, 15, '<?php\n\nnamespace App\\Http\\Controllers;\n\nuse App\\Http\\Controllers\\Controller;\nuse Request;\nuse DB;\n\nclass GroupsController extends Controller\n{\n\n    public function getView($id){\n      session_start();\n      $get_group = DB::table(''groups'')->where(''id'',''='',$id)->get();\n      if($get_group != null){\n        $_SESSION[''group_id''] = $id;\n        return view(''grupo'');\n      }\n    }\n\n    public function get_info(){\n      session_start();\n      $get_group = DB::table(''groups'')->where(''id'',''='',$_SESSION[''group_id''])->get();\n      return $get_group;\n    }\n\n    public function get_members(){\n      session_start();\n      $get_members = DB::table(''groups_members'')\n      ->join(''users'',''groups_members.member_id'',''='',''users.id'')\n      ->join(''user-data'',''groups_members.member_id'',''='',''user-data.user_id'')\n      ->select(''users.name'',''users.id'',''users.email'',''user-data.profile_picture'')\n      ->where(''groups_members.group_id'',''='',$_SESSION[''group_id''])->get();\n      return $get_members;\n    }\n\n    public function addGroup(){\n      session_start();\n      $name = Request::input(''nombre'');\n      $asunto = Request::input(''asunto'');\n      $insertar_grupo = DB::table(''groups'')->insertGetId([\n        ''name'' => $name,\n        ''type'' => $asunto,\n        ''created_by'' => $_SESSION[''uid''],\n      ]);\n\n      DB::table(''groups_members'')->insert([\n        ''group_id'' => $insertar_grupo,\n        ''member_id'' => $_SESSION[''uid''],\n      ]);\n\n      return 200;\n    }\n\n    public function get_myGroups(){\n      session_start();\n      $misGrupos = DB::table(''groups_members'')\n      ->join(''groups'',''groups_members.group_id'',''='',''groups.id'')\n      ->select(''groups.name'',''groups_members.member_id'',''groups.id'')\n      ->where(''member_id'',''='',$_SESSION[''uid''])->get();\n\n      return $misGrupos;\n    }\n\n    public function get_posts(){\n      session_start();\n      $publicaciones = DB::table(''grupos-posts'')\n      ->join(''users'',''grupos-posts.user_id'',''='',''users.id'')\n      ->join(''user-data'',''grupos-posts.user_id'',''='',''user-data.user_id'')\n      ->select(''grupos-posts.texto'',''grupos-posts.grupo_id'',''users.name'',''users.email'',''user-data.profile_picture'')\n      ->where(''grupos-posts.grupo_id'',''='',$_SESSION[''group_id''])\n      ->get();\n      return $publicaciones;\n    }\n\n    public function do_post(){\n      session_start();\n      $mensaje = Request::input(''mensaje'');\n      $valida = DB::table(''groups_members'')->where(''group_id'', ''='',$_SESSION[''group_id''])\n      ->where(''member_id'', ''='',$_SESSION[''uid''])->get();\n\n      if($valida != null){\n        $publicar = DB::table(''grupos-posts'')\n        ->insert([\n          ''user_id'' => $_SESSION[''uid''],\n          ''texto'' => $mensaje,\n          ''grupo_id'' => $_SESSION[''group_id''],\n        ]);\n        return 200;\n      }\n\n      return 404;\n    }\n\n    public function getNotas(){\n      session_start();\n      $notas = DB::table(''grupos-notas'')->select(''nota'',''id'')->where(''grupo_id'',''='',$_SESSION[''group_id''])->get();\n      return $notas;\n    }\n\n    public function eliminarNota(){\n      $id = Request::input(''nota_id'');\n      DB::table(''grupos-notas'')->where(''id'',''='',$id)->delete();\n      return 400;\n    }\n\n    public function agregarNota(){\n      session_start();\n      $nota = Request::input(''nota'');\n      $valida = DB::table(''groups_members'')->where(''group_id'', ''='',$_SESSION[''group_id''])\n      ->where(''member_id'', ''='',$_SESSION[''uid''])->get();\n\n\n      if($valida != null){\n        $insertar = DB::table(''grupos-notas'')->insert([\n          ''user_id'' => $_SESSION[''uid''],\n          ''nota'' => $nota,\n          ''grupo_id'' => $_SESSION[''group_id''],\n        ]);\n        return 200;\n      }\n      return 404;\n    }\n\n    public function postCode(){\n      session_start();\n      $titulo = Request::input(''titulo'');\n      $codigo = Request::input(''codigo'');\n      $sintaxis = Request::input(''sintaxis'');\n\n      $valida = DB::table(''groups_members'')->where(''group_id'', ''='',$_SESSION[''group_id''])\n      ->where(''member_id'', ''='',$_SESSION[''uid''])->get();\n\n      if($valida != null){\n        $insertar = DB::table(''post-codigos'')->insert([\n          ''user_id'' => $_SESSION[''uid''],\n          ''codigo'' => $codigo,\n          ''sintaxis'' => $sintaxis,\n          ''titulo'' => $titulo,\n          ''grupo_id'' => $_SESSION[''group_id''],\n        ]);\n        return 200;\n      }\n      return 404;\n    }\n\n    public function getCodes(){\n      session_start();\n\n            $code_post = DB::table(''post-codigos'')\n            ->join(''users'',''post-codigos.user_id'',''='',''users.id'')\n            ->join(''user-data'',''users.id'',''='',''user-data.user_id'')\n            ->select(''post-codigos.id'',''post-codigos.sintaxis'',''post-codigos.titulo'',\n            ''users.name'',''user-data.profile_picture'',''post-codigos.user_id'',''post-codigos.grupo_id''\n            )->where(''post-codigos.grupo_id'',$_SESSION[''group_id''])\n            ->orderBy(''post-codigos.id'',''desc'')->get();\n\n            return $code_post;\n    }\n\n    public function addMember(){\n      session_start();\n      $user = Request::input(''usuario'');\n      $validar  = DB::table(''users'')->where(''email'',$user)->get();\n      if ($validar != null) {\n        $insertar = DB::table(''groups_members'')->insert([\n          ''group_id'' => $_SESSION[''group_id''],\n          ''member_id'' => $validar[0]->id,\n        ]);\n        return 200;\n      }\n      else {\n        return 404;\n      }\n    }\n\n}', 'php', 'IMController', NULL),
-(11, 8, 'printf("funciona")', 'c', 'Funciona!', 3);
+(11, 8, 'printf("funciona")', 'c', 'Funciona!', 3),
+(12, 23, 'console.log(1)', 'javascript', 'console.log(1)', NULL),
+(13, 23, 'printf("funciona")', 'c', '', 3),
+(14, 23, 'console.log(1)', 'c', 'console', NULL),
+(15, 23, 'printf("funciona")', 'c', 'printf', 3);
 
 -- --------------------------------------------------------
 
@@ -302,7 +315,16 @@ INSERT INTO `posts` (`id`, `id_user`, `text`, `mencion`, `likes`, `created_at`, 
 (65, 8, 'dgrt', 8, 3, '2016-09-15 16:33:20', NULL),
 (66, 8, 'ytyty', 15, 16, '2016-09-15 16:34:58', NULL),
 (67, 8, 'fgth', 11, 0, '2016-09-15 16:35:47', NULL),
-(68, 8, 'Yo pienso que es una cosa mala , negativa, que quieren matar a ubu, #noalaactualización', 8, 0, '2016-09-15 16:53:27', NULL);
+(68, 8, 'Yo pienso que es una cosa mala , negativa, que quieren matar a ubu, #noalaactualización', 8, 1, '2016-09-15 16:53:27', NULL),
+(69, 23, 'Hola amigos!', 23, 0, '2016-10-23 02:16:59', NULL),
+(70, 23, 'lololo', 8, 0, '2016-10-23 02:18:32', NULL),
+(71, 23, '', 23, 0, '2016-10-23 02:27:54', NULL),
+(72, 23, '', 23, 0, '2016-10-23 02:27:58', NULL),
+(73, 23, 'hola', 23, 0, '2016-10-23 02:28:25', NULL),
+(74, 23, 'ulala', 8, 0, '2016-10-23 02:29:11', NULL),
+(75, 23, 'holaa', 23, 0, '2016-10-23 02:38:42', NULL),
+(76, 23, 'console.log (1)', 23, 0, '2016-10-23 02:39:13', NULL),
+(77, 23, 'hola', 8, 0, '2016-10-23 02:39:55', NULL);
 
 -- --------------------------------------------------------
 
@@ -334,7 +356,10 @@ INSERT INTO `suscribciones` (`id`, `suscriptor_id`, `suscripcion_id`) VALUES
 (14, 15, 8),
 (15, 8, 15),
 (16, 11, 15),
-(17, 10, 15);
+(17, 10, 15),
+(18, 23, 11),
+(19, 23, 8),
+(20, 8, 23);
 
 -- --------------------------------------------------------
 
@@ -371,7 +396,8 @@ INSERT INTO `user-data` (`id`, `user_id`, `profile_picture`, `profile_cover`, `T
 (15, 19, 'users-data/profile-pictures/defaultpicture.jpeg', 'users-data/profile-cover/defaultcover.jpg', NULL, NULL, NULL, NULL),
 (16, 20, 'users-data/profile-pictures/defaultpicture.jpeg', 'users-data/profile-cover/defaultcover.jpg', NULL, NULL, NULL, NULL),
 (17, 21, 'users-data/profile-pictures/defaultpicture.jpeg', 'users-data/profile-cover/defaultcover.jpg', NULL, NULL, NULL, NULL),
-(18, 22, 'users-data/profile-pictures/defaultpicture.jpg', 'users-data/profile-cover/defaultcover.jpg', NULL, NULL, NULL, NULL);
+(18, 22, 'users-data/profile-pictures/defaultpicture.jpg', 'users-data/profile-cover/defaultcover.jpg', NULL, NULL, NULL, NULL),
+(19, 23, 'users-data/profile-pictures/defaultpicture.jpeg', 'users-data/profile-cover/defaultcover.jpg', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -405,7 +431,8 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `creat
 (18, 'Rene Elizondo', 'rene@gmail.com', '93de1a7f9a00f8823ac377738b66236b', NULL, NULL, NULL),
 (20, 'Francisco Torres', 'torres@hotmail.com', 'b50ac41ec20631c7b6be72f070d8ff67', NULL, NULL, NULL),
 (21, '', '', 'd41d8cd98f00b204e9800998ecf8427e', NULL, NULL, NULL),
-(22, 'Lucero Moran', 'lucerom@hotmail.com', '4c54766ed4f728b3929385200d5ad28e', NULL, NULL, NULL);
+(22, 'Lucero Moran', 'lucerom@hotmail.com', '4c54766ed4f728b3929385200d5ad28e', NULL, NULL, NULL),
+(23, 'prueba', 'prueba@prueba.com', 'c893bad68927b457dbed39460e6afd62', NULL, NULL, NULL);
 
 --
 -- Índices para tablas volcadas
@@ -493,22 +520,22 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `comentarios-codigos`
 --
 ALTER TABLE `comentarios-codigos`
-  MODIFY `id` bigint(80) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` bigint(80) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 --
 -- AUTO_INCREMENT de la tabla `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT de la tabla `groups_members`
 --
 ALTER TABLE `groups_members`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 --
 -- AUTO_INCREMENT de la tabla `grupos-notas`
 --
 ALTER TABLE `grupos-notas`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT de la tabla `grupos-posts`
 --
@@ -518,32 +545,32 @@ ALTER TABLE `grupos-posts`
 -- AUTO_INCREMENT de la tabla `likes`
 --
 ALTER TABLE `likes`
-  MODIFY `interaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
+  MODIFY `interaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
 --
 -- AUTO_INCREMENT de la tabla `post-codigos`
 --
 ALTER TABLE `post-codigos`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT de la tabla `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
 --
 -- AUTO_INCREMENT de la tabla `suscribciones`
 --
 ALTER TABLE `suscribciones`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 --
 -- AUTO_INCREMENT de la tabla `user-data`
 --
 ALTER TABLE `user-data`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
